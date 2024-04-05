db1a9f281c8d377023eca97c3d342e3993697dcc
Lucas <lgariador.freelance@play365.io>
2024-04-05 12:59:58 -0300

    Initial commit

---
+++ src/main/java/com/lucasgariador/springbootwebchr/SpringbootWebdbApplication.java
@@ -0,0 +1,14 @@
+package com.lucasgariador.springbootwebchr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWebdbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootWebdbApplication.class, args);
+		System.out.println("Hello World");
+	}
+
+}
---
+++ src/test/java/com/lucasgariador/springbootwebchr/SpringbootWebdbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucasgariador.springbootwebchr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootWebdbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
